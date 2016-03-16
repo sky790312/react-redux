d6a440fcd507d5d7d187832bc0b24e05f65a0238
kevin hu <sky790312yks@gmail.com>
2016-03-16 16:23:45 +0800

    continue sparate login file

--- src/main.js
+++ src/main.js
@@ -3,63 +3,12 @@ import Redux, { createStore, combineReducers, applyMiddleware } from 'redux';
 // combineReducers => 合併 reducers
 // applyMiddleware => Middleware 處理非同步ex: ajax
 
+import CountButton from 'component/CountButton';
+
 let button = document.getElementById('mybutton');
 let button2 = document.getElementById('mybutton2');
 let button3 = document.getElementById('mybutton3');
 let text = document.getElementById('count');
-
-//=========action part - 分離至ex: actions/count.js =============//
-
-const CLICKEVENT = 'CLICKEVENT';
-function clickAction(count = 1, delay = 0) {
-    return{
-        type: CLICKEVENT,
-        count,
-        delay
-    };
-}
-
-//========= middleware part 分離至ex: middleware.js =============//
-
-// var inc = (x) => x+1;
-// equal to
-// var inc = function (x) { return x + 1; };
-const myDelayMiddleware = store => next => action => {
-    if (action.delay) {
-        return setTimeout(() => {
-            next(action);
-        }, action.delay * 1000);
-    } else {
-        next(action);
-    }
-}
-
-//========= reducer part - 分離至ex: reducers/countReducer.js =============//
-
-let initialState = {
-    clickCount: 0
-};
-function myClickReducer (state = initialState, action) {
-    switch (action.type) {
-        case CLICKEVENT:
-            state.clickCount += action.count;
-            // 展開運算子(Spread Operator)與其餘參數(Rest parameters)
-            return { ...state };
-        default:
-            return state;
-    }
-}
-
-//========= store part =============//
-
-// deal with middleware
-let createStoreWithMiddleware = applyMiddleware(myDelayMiddleware)(createStore);
-// 合併 reducers
-let myReducers = combineReducers({
-    myClickState: myClickReducer
-});
-// 最後合併 widdleware & reducers
-let store = createStoreWithMiddleware(myReducers);
 
 //========= view part 分離至ex: 結合react(Provider) - xxx.jsx =============//
 
